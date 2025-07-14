898f9c1f287be8e5cf79e3740cefda72c63c3119
Amalia Prawinda <amalia.a.prawinda@gmail.com>
2025-07-14 20:35:25 +0700

    main image light box

--- src/components/ProductLightBox.jsx
+++ src/components/ProductLightBox.jsx
@@ -26,7 +26,27 @@ const ProductLightBox = ({
 
   return (
     <div className='fixed inset-0 bg-black/70 z-50 flex items-center justify-center'>
-      
+      <div className='relative bg-transparent rounded-2xl p-4 flex flex-col items-center'>
+        <button
+            className='absolute top-[-26px] right-[88px]'
+            onClick={onClose}
+          >
+            <img src={close} alt='Close Button' />
+          </button>
+        <div className='flex items-center gap-4 z-10'>
+          <button onClick={prevImg}>
+            <img src={leftArrow} alt='Left Arrow' className='transform -translate-x-[-46px]' />
+          </button>
+          <img
+            src={images[activeIndex]}
+            alt={`Main Product ${activeIndex + 1}`}
+            className='w-[500px] h-[500px] object-cover rounded-xl'
+          />
+          <button onClick={nextImg}>
+            <img src={rightArrow} alt='Right Arrow' className='transform -translate-x-[46px]' />
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
